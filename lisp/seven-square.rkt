6a294fe0f339fb02d6b0a80c05a8d0cb8a1a7cfb
Yang Hong <yanghong@thundersoft.com>
2013-06-04 14:02:37 +0800

    Use slideshow/pict to gain better scale performance
    50~60% cpu time (flomap->bitmap (flomap->scale (bitmap->flomap bm)))
    8~16% cpu time (pict->bitmap (scale bm ratio))
    
    Also use openssl/sha1 to calculate checksum to avoid unneed update
    
    Implemented an CRC16 checksum algorithm, it slower than sha1,
    cusume 20% cpu time.

--- lisp/seven-square.rkt
+++ lisp/seven-square.rkt
@@ -2,9 +2,15 @@
 
 (require "adb.rkt")
 (require images/flomap)
+(require slideshow/pict)
+(require openssl/sha1)
 
 (define fb-capture-timer 1)
-(define do-scale #f)
+(define do-scale-flomap #f)
+(define do-scale-pict #t)
+(define do-scale (or do-scale-flomap do-scale-pict))
+(define do-fb-checksum-crc16 #f)
+(define do-fb-checksum-sha1 #t)
 
 (define sq-frame%
   (class frame%
@@ -14,17 +20,18 @@
       (send fb-capture-timer stop))
     (augment on-close)))
 
-(define frame (new sq-frame% [label "Seven Square"]))
+(define xframe (new sq-frame% [label "Seven Square"]))
 
 (define WIDTH 360)
 (define HEIGHT 600)
 (define BPP 4)
 
-(define viewer (new canvas% [parent frame]
+(define viewer (new canvas% [parent xframe]
                     [min-width WIDTH]
                     [min-height HEIGHT]))
 
-(define (showf) (send frame show #t))
+(define (showf) (send xframe show #t))
+(define (resize-frame w h) (send xframe resize w h))
 
 (define adb (new adb%))
 
@@ -55,18 +62,53 @@
                     (le32->number (read-bytes 4 in)))))
   (fbinfo a b c d e f g h i j k l m))
 
-(define fbbitmap (make-object bitmap%
-                   1 1 #f #t))
+(define fbbitmap (make-object bitmap% 1 1 #f #t))
+(define fbhash 1024)
 
 (define (update-bitmap-size w h)
-  (when (or (not (equal? w (send fbbitmap get-width)))
-            (not (equal? h (send fbbitmap get-height))))
-    (set! fbbitmap (make-object bitmap%
-                     w h #f #t))
-  (when (not do-scale)
-    (displayln "Resize window")
-    (send frame resize w h))
+  (unless (and (equal? w (send fbbitmap get-width))
+               (equal? h (send fbbitmap get-height)))
+    (set! fbbitmap (make-object bitmap% w h #f #t))
+    ; FIXME: windows not scaled as excepted
+    (when do-scale
+      (displayln "Resize window")
+      (resize-frame w h)
+      (set! fbhash "reset"))
     ))
+
+(define crctbl (list
+                #x0000 #x1081 #x2102 #x3183
+                #x4204 #x5285 #x6306 #x7387
+                #x8408 #x9489 #xa50a #xb58b
+                #xc60c #xd68d #xe70e #xf78f))
+
+(define (crc16 data len)
+  (define (cacl crc c)
+    (bitwise-xor
+     (bitwise-and (arithmetic-shift crc -4) #xffff)
+     (list-ref crctbl (bitwise-and (bitwise-xor crc c) 15))))
+  (let loop ([i 0] [crc #xffff])
+    (cond
+      [(= i len)
+       (bitwise-and (bitwise-not crc) #xffff)]
+      [else
+       (loop (add1 i) (cacl crc (bytes-ref data i)))])))
+
+(define (content-changed? raw size)
+  (define newhash
+    (cond
+      [do-fb-checksum-crc16 (crc16 raw size)]
+      [do-fb-checksum-sha1
+       (define in (open-input-bytes raw))
+       (define hash (sha1 in))
+       (close-input-port in)
+       hash]
+      [else fbhash]))
+  (define changed (not (equal? fbhash newhash)))
+  (when changed
+    ;(displayln newhash)
+    (set! fbhash newhash))
+  changed)
 
 (define fb-callback
   (lambda (in out)
@@ -81,22 +123,25 @@
     (define (draw raw)
       (update-bitmap-size width height)
       (send fbbitmap set-argb-pixels 0 0 width height
-            ; Fake rgba to argbraw
+            ; Fake rgba to argbraw, potential perf issue?
             (bytes-append (make-bytes 1 255) raw))
-      (cond
-        [do-scale
-         (define floimg (bitmap->flomap fbbitmap))
-         (define scaled (flomap->bitmap
-                         (flomap-scale floimg
-                                       (/ WIDTH width)
-                                       (/ HEIGHT height))))
-         (send (send viewer get-dc)
-               draw-bitmap scaled 0 0)]
-        [else
-         (send (send viewer get-dc)
-               draw-bitmap fbbitmap 0 0)]))
+      (send (send viewer get-dc) draw-bitmap
+            (cond
+              [do-scale-flomap
+               (flomap->bitmap
+                (flomap-scale (bitmap->flomap fbbitmap)
+                              (/ WIDTH width)
+                              (/ HEIGHT height)))]
+              [do-scale-pict
+               (pict->bitmap
+                (scale (bitmap fbbitmap)
+                       (/ WIDTH width)))]
+              [else fbbitmap])
+            0 0))
     (cond
-      [(equal? size (bytes-length raw)) (draw raw)]
+      [(equal? size (bytes-length raw))
+       (when (content-changed? raw size)
+         (draw raw))]
       [else (displayln "Dropped currupted frame")])
     ))
 
