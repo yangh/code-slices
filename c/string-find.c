fd9d31fc27469209c9e4d3bb43025b49bb4b77f8
xiayang0705 <xiayang0705@thundersoft.com>
2018-05-23 17:51:13 +0800

    string-find add string compare func

--- c/string-find.c
+++ c/string-find.c
@@ -11,11 +11,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if 1
+#if 0
  #define DEBUG printf
 #else
  #define DEBUG(...)
 #endif
+
+int string_len(const char *str)
+{
+	int len = 0;
+
+	while (str != NULL && *str != '\0') {
+		str++;
+		len++;
+	}
+
+	return len;
+}
+
+int string_compare(const char* str1, const char* str2, size_t len)
+{
+	int n = len;
+
+	if (str1 == NULL || str2 == NULL) {
+		return -1;
+	}
+
+	while (*str1 != '\0' &&
+		   *str2 != '\0' &&
+		   n > 0 &&
+		   *str1 == *str2) {
+		str1++;
+		str2++;
+		n--;
+	}
+
+	if (n == 0) {
+		if (*str1 == '\0') {
+			/* Equal */
+			return 0;
+		} else {
+			/* Lager than */
+			return 1;
+		}
+	} else {
+		/* less than */
+		return -1;
+	}
+}
+
+typedef int(cmp_func)(const char*, const char*, size_t);
+
+void TEST_CMP(cmp_func c1, cmp_func c2, const char *str1, const char *str2)
+{
+	int ret1, ret2;
+
+	ret1 = c1(str1, str2, string_len(str2));
+    ret2 = c2(str1, str2, string_len(str2));
+	printf("Compare %s, %s, %d, %d, %s\n", str1, str2, ret1, ret2, (ret1 == ret2)?"PASS":"FAIL");
+}
 
 int string_find_recurse (const char *str, const char *dst, size_t pos, int str_len, int dst_len, int target_len)
 {
@@ -50,8 +104,8 @@ int string_find (const char *str, const char *dst)
 	int pos = -1;
 	size_t slen, dlen;
 
-	slen = strlen(str);
-	dlen = strlen(dst);
+	slen = string_len(str);
+	dlen = string_len(dst);
 
 	if (slen < dlen) {
 		return pos;
@@ -83,6 +137,12 @@ int main(int argc, char *argv[])
 	printf("Find %s in %s, at: %d\n", str, str6, string_find(str, str6));
 	printf("Find %s in %s, at: %d\n", str, str7, string_find(str, str7));
 	printf("Find %s in %s, at: %d\n", str, str8, string_find(str, str8));
+
+	TEST_CMP(strncmp, string_compare, "", "");
+	TEST_CMP(strncmp, string_compare, "abcdef", "abcdef");
+	TEST_CMP(strncmp, string_compare, "abcdef", "abc");
+	TEST_CMP(strncmp, string_compare, "abcdef", "def");
+	TEST_CMP(strncmp, string_compare, "abcdef", "abcdefghijk");
 
 	return 0;
 }
